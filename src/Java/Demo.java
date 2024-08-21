bd69da8fce97882978a1ef3f94830f46a59c64ce
AsifJamadar352 <asifjamadar352@gmail.com>
2024-08-21 16:01:44 +0530

    third commit

--- src/Java/Demo.java
+++ src/Java/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         int x = 20;
         int y = 35;
+        System.out.println("asif jamadar");
         System.out.println(x + ", " + y);
     }
 }
